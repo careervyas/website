50efe453d9806440e4889874acd217cd6c1e2875
Aditya Gupta <72199742+Adityagupta1625@users.noreply.github.com>
2022-11-29 22:21:20 +0530

    sitemap setup

---
+++ pages/next-sitemap.js
@@ -0,0 +1,14 @@
+const siteUrl = "https://www.careervyas.com/";
+
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    
+    additionalSitemaps: [
+      `${siteUrl}/sitemap.xml`,
+      `${siteUrl}/server-sitemap.xml`,
+    ],
+  },
+  exclude: ["/secret"],
+};
